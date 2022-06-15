1fb0faf670152417d72858ee9bebe93ed7fea336
dukunhong <158034623@qq.com>
2022-06-15 20:12:24 +0800

    push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello.git4!");
         System.out.println("mater test !");
         System.out.println("hot-fix test !");
+        System.out.println("push test !");
     }
 }
